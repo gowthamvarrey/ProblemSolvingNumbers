435399acb48a6472f54438b235b6fc7547b90382
gowthamvarrey <141480204+gowthamvarrey@users.noreply.github.com>
2025-03-30 22:08:17 +0530

    Modified secondMax Code

--- secondMax.py
+++ secondMax.py
@@ -1,14 +1,15 @@
 arr = [1, 2, 4, 8, 8]
 firstMax = float('-inf')
-for i in arr:
-    if i > firstMax:
-        firstMax = i
-print(firstMax)
 secondMax = float('-inf')
 for i in arr:
-    if firstMax > i > secondMax:
+    if i > firstMax:
+        secondMax = firstMax
+        firstMax = i
+    elif i > secondMax and i != firstMax:
         secondMax = i
-print(secondMax)
+print(firstMax,secondMax)
+
+
 
 
 
